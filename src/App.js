b18c72a647506a51359697d6085a0c664a311d27
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-22 20:15:13 -0700

    updated app.j swith nav and footer bar. also created Home page

--- src/App.js
+++ src/App.js
@@ -5,52 +5,68 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 // these are the "page links" for your application 
 import About from './pages/About'
 import Home from './pages/Home'
-import { 
-  Collapse, 
-  Navbar, 
-  NavbarToggler, 
-  NavbarBrand, 
-  Nav, 
-  NavItem, 
-  NavLink } from 'reactstrap';
-  import './App.css'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap'
+import './App.css'
 
 const App = () => {
 
   const [collapsed, setCollapsed] = useState(true);
 
   const toggleNavbar = () => setCollapsed(!collapsed);
-  
-  return (
-    // recommend by ReactRouter.com to start with the 'Router' tag, followed by a 'div tag'
-    <Router>
-      <div>
-      <Navbar color="faded" light>
-        <NavbarBrand>Nicholas Paul Ruiz Dallas ~ Full Stack Web Developer</NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink> <Link className="link" to="/">Home</Link></NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink> <Link className="link" to="/About">Contact Page</Link></NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="link" href="https://github.com/nicholasd-uci">GitHub</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
 
-{/* This is a switch elem that define routes for our application- On line 4 its being called  */}
-      <Switch>
-{/* These are the routs that we will be defining */}
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-      </Switch>
-      </div>
-    </Router>
+  return (
+    <>
+      {/* recommend by ReactRouter.com to start with the 'Router' tag, followed by a 'div tag'  */}
+      <Router>
+        <div>
+          <Navbar color="faded" light>
+            <NavbarBrand>Nicholas Paul Ruiz Dallas ~ Full Stack Web Developer</NavbarBrand>
+            <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+            <Collapse isOpen={!collapsed} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink> <Link className="link" to="/">Home</Link></NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink> <Link className="link" to="/About">Contact Page</Link></NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="link" href="https://github.com/nicholasd-uci">GitHub</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+
+          {/* This is a switch elem that define routes for our application- On line 4 its being called  */}
+          <Switch>
+            {/* These are the routs that we will be defining */}
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+
+          <br /><br /><br /><br /><br />
+
+        </div>
+      </Router>
+{/* This is where the foot needs to be on every page. */}
+      <section>
+        <footer class="footer">
+          <div class="container">
+            <span class="text-muted"> These test here need to be in the footer</span>
+          </div>
+        </footer>
+      </section>
+
+    </>
+
   )
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,60 @@
 import React from 'react'
+import { Jumbotron, Container } from 'reactstrap'
+import {
+  Card, Button, CardImg, CardTitle, CardText, CardGroup,
+  CardSubtitle, CardBody
+} from 'reactstrap'
+import './style.css'
 
 const Home = () => {
   return (
-    <h1>This should render all of your portfolio work!</h1>
+    <>
+      <body className="background">
+        <div>
+          <Jumbotron fluid>
+            <Container fluid>
+              <h1 className="display-3">Fluid jumbotron</h1>
+              <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </Container>
+          </Jumbotron>
+        </div>
+        <br/><br/><br/>
+        <div>
+          {/*  portfolio is going to go here and we will be ale to see our projects along with photo of me  */}
+          <CardGroup>
+            <Card>
+              <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+            <Card>
+              <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+            <Card>
+              <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </CardGroup>
+        </div>
+        <br/><br/><br/>
+      </body>
+
+    </>
   )
 }
 
