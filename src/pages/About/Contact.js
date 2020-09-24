f6c2c1f3ac8b7df5985ec150a9f5d394a2b206bb
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-23 23:24:04 -0700

    updated contact page with basic outline

--- src/pages/About/Contact.js
+++ src/pages/About/Contact.js
@@ -5,37 +5,18 @@ const Contact = (props) => {
   return (
     <Container>
       <Row>
-        <Col>.col</Col>
+        <Col> This will be a short bio about my self and what I have done work wise and what I have exp in the work field and how I want to bring that to ER and be able to better leave a carbon foot print for a company! </Col>
+      </Row>
+      <hr/>
+      <Row>
+        <Col>Other Portfolio HTML page Link </Col>
+        <Col>HBYL and WWD info</Col>
+        <Col>Phone and Email Here</Col>
+        <Col>PDF of Resume Here</Col>
       </Row>
       <Row>
-        <Col>.col</Col>
-        <Col>.col</Col>
-        <Col>.col</Col>
-        <Col>.col</Col>
-      </Row>
-      <Row>
-        <Col xs="3">.col-3</Col>
-        <Col xs="auto">.col-auto - variable width content</Col>
-        <Col xs="3">.col-3</Col>
-      </Row>
-      <Row>
-        <Col xs="6">.col-6</Col>
-        <Col xs="6">.col-6</Col>
-      </Row>
-      <Row>
-        <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-        <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-        <Col sm="4">.col-sm-4</Col>
-      </Row>
-      <Row>
-        <Col sm={{ size: 6, order: 2, offset: 1 }}>.col-sm-6 .order-sm-2 .offset-sm-1</Col>
-      </Row>
-      <Row>
-        <Col sm="12" md={{ size: 6, offset: 3 }}>.col-sm-12 .col-md-6 .offset-md-3</Col>
-      </Row>
-      <Row>
-        <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
-        <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
+        <Col xs="3">Photo Here</Col>
+        <Col sm={{ size: 'auto', offset: 1 }}>Social Media App's will go here</Col>
       </Row>
     </Container>
   );
